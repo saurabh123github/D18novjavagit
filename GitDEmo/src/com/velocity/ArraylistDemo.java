13a6a1ce526b7a27d04b8cd3537e42f235ca672f
saurabh <sksaurabhkshirsagar@gmail.com>
2024-03-05 17:48:31 +0530

    added arraylist code for number

--- GitDEmo/src/com/velocity/ArraylistDemo.java
+++ GitDEmo/src/com/velocity/ArraylistDemo.java
@@ -1,5 +1,15 @@
 package com.velocity;
 
+import java.util.ArrayList;
+
 public class ArraylistDemo {
+	public static void main(String[] args) {
+		ArrayList<String> al=new ArrayList<String>();
+		al.add("pune");
+		al.add("mimbai");
+		al.add("benglore");
+		al.add("latur");
+		System.out.println(al);
+	}
 
 }
---
+++ GitDEmo/src/com/velocity/ArraylistDemo2.java
@@ -0,0 +1,15 @@
+package com.velocity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArraylistDemo2 {
+	public static void main(String[] args) {
+		List<Integer> list=new ArrayList<Integer>();
+		list.add(10);
+		list.add(20);
+		list.add(30);
+		list.add(40);
+		System.out.println(list);
+	}
+}
