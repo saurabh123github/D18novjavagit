5edf0ea34d8daeb64dfec322f57f7331dac91727
saurabh <sksaurabhkshirsagar@gmail.com>
2024-03-06 00:51:03 +0530

    Arralist wit name is added to local repositoy

---
+++ GitDemo1/src/com/velocity/ArrayListDemo.java
@@ -0,0 +1,16 @@
+package com.velocity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListDemo {
+	public static void main(String[] args) {
+		List<String> list=new ArrayList<String>();
+		list.add("Saurabh");
+		list.add("amar");
+		list.add("Akshay");
+		list.add("Pradip");
+		System.out.println(list);
+	}
+
+}
